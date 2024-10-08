edeb0088586c5203f37603112a3c72b99418d768
nix@rif <arif@nixs-MacBook-Pro.local>
2024-10-08 08:26:29 +0800

    Remove Image Product Components

--- client/app/products/Products.tsx
+++ client/app/products/Products.tsx
@@ -81,12 +81,13 @@ const Products = () => {
               className="border shadow rounded-md p-4 max-w-full w-full mx-auto"
             >
               <div className="flex flex-col items-center">
-                <Image
+                img
+                {/* <Image
                   alt={product.name}
                   width={150}
                   height={150}
                   className="mb-3 rounded-2xl w-36 h-36"
-                />
+                /> */}
                 <h3 className="text-lg text-gray-900 font-semibold">
                   {product.name}
                 </h3>
